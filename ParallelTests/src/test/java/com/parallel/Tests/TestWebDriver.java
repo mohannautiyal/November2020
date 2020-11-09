2e090512e2da0cda2f3685fc5d20e9c7f013d447
mohannautiyal <mohannautiyal80@gmail.com>
2020-11-09 22:38:06 +0530

    WebDriver tests added

---
+++ ParallelTests/src/test/java/com/parallel/Tests/TestWebDriver.java
@@ -0,0 +1,58 @@
+package com.parallel.Tests;
+
+import static org.testng.Assert.assertTrue;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class TestWebDriver {
+	
+	
+	WebDriver driver;
+	
+	@BeforeMethod
+	public void Setup() {
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();
+		driver.get("https://opensource-demo.orangehrmlive.com/index.php/auth/login");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+		System.out.println("Launching Application..");
+	}
+	
+	@Test
+	public void verifyAppLaunch() {
+	 List<WebElement> logoelem=	driver.findElements(By.id("divLogo"));	 
+	 assertTrue(logoelem.size()>0, "Logo not present");
+	 System.out.println("Logo verification passed");
+	}
+	
+	
+	@Test
+	public void verifyAppLogin() {
+	 	driver.findElement(By.id("txtUsername")).sendKeys("Admin");	 
+	 	driver.findElement(By.id("txtPassword")).sendKeys("admin123");	 
+	 	driver.findElement(By.id("btnLogin")).click();	 
+
+	 int elem=driver.findElements(By.xpath("//h1[contains(text(),'Dashboard')]")).size();
+	 assertTrue(elem>0, "Login failed");
+	 System.out.println("Login test successfull");
+	}
+	
+	@AfterMethod
+	public void quit() {
+		System.out.println("Closing Browser...");
+		driver.quit();
+	}
+
+}
