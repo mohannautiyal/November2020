d619cf0c2c8096b3d02b6dc38ffb1fbefceca085
mohannautiyal <mohannautiyal80@gmail.com>
2020-11-17 23:04:06 +0530

    ENUM basic

---
+++ ENUMProj/src/com/home/EnumExample.java
@@ -0,0 +1,13 @@
+package com.home;
+
+public class EnumExample {
+
+	enum lang{
+		ENGLISH,HINDI,FRENCH,URDU,KOREAN,JAPANEESE
+	}
+	
+	public static void main(String[] args) {
+		System.out.println(lang.HINDI);
+		
+	}
+}
